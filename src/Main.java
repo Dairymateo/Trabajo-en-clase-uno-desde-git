a3a99c4cc368f219be90b00eec752662cd9962c4
Mateo Herrera <105869739+Dairymateo@users.noreply.github.com>
2023-04-12 11:37:56 -0500

    ya con el void
    xd

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,30 @@
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Empleado e1 =new Empleado();
-        Scanner sc= new Scanner(System.in);
+        // Press Alt+Intro with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        Empleado e1=new Empleado();
+        Scanner sc=new Scanner(System.in);
 
         System.out.println("Ingrese su nombre: ");
-        String nombre=sc.next();
+        String nombre=sc.nextLine();
+        System.out.println("-----------------------------");
         System.out.println("Ingrese su cedula: ");
         String cedula=sc.next();
+        System.out.println("-----------------------------");
         System.out.println("Ingrese las horas trabajadas: ");
-        int horasTrabajadas=sc.nextInt();
+        int horasTrabajadas= Integer.parseInt(sc.next());
+        System.out.println("-----------------------------");
         System.out.println("Ingrese el valor por hora de trabajo: ");
-        double valorHora=sc.nextDouble();
+        double valorHora= Double.parseDouble(sc.next());
+        System.out.println("-----------------------------");
         System.out.println("Ingrese el porcentaje de la retencion en decimales: ");
-        double retencionFuente= sc.nextDouble();
+        double retencionFuente=Double.parseDouble(sc.next());
+        System.out.println("-----------------------------");
         e1.setNombre(nombre);
         e1.setCedula(cedula);
-        e1.setHorasTrabajadas(horasTrabajadas);
+        e1.sethorasTrabajadas(horasTrabajadas);
         e1.setvalorHora(valorHora);
         e1.setRetencionFuente(retencionFuente);
         System.out.println("El empleado: "+e1.getNombre());
@@ -29,12 +34,12 @@ public class Main {
         System.out.println("El salario neto es: "+e1.calcularSalarioNeto());
 
         System.out.println("Ingrese las horas extras");
-        int horasExtras= sc.nextInt();
-        System.out.println("Las horas totales son: "+e1.calcularHorasTrabajadas(horasExtras));
+        int horasExtras= Integer.parseInt(sc.next());
+        System.out.println("-----------------------------");
+        e1.adicionarHorasExtras(horasExtras);
+        System.out.println("Las horas totales son: " +e1.gethorasTrabajadas());
         System.out.println("El nuevo salario neto es: "+e1.calcularSalarioNeto());
 
+        }
+    }
 
-
-
-     }
- }
